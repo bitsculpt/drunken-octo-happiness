5fbb64a4b2f6278e029f65691e588f135b25fd1f
David Williams <davew1982@gmail.com>
2014-01-03 20:51:19 -0500

    made all tests pass

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,2 +1,24 @@
 class BuildingsController < ApplicationController
+  def index
+  end
+  def new
+    @building = Building.new
+  end
+  
+  def create
+    @building = Building.new(building_params)
+    if @building.save
+      redirect_to @building, notice: 'Building was successfully created.'
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @building = Building.find(params[:id])
+  end
+
+  def building_params
+  params.require(:building).permit(:street_address, :city, :state, :postal_code, :description)
+  end
 end
