86aca28dea522da67d559509da83362fa1ea334f
David Williams <davew1982@gmail.com>
2014-01-04 00:40:59 -0500

    Added nullify dependency for Owner model

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,6 +2,6 @@ class Owner < ActiveRecord::Base
   validates_presence_of :first_name
   validates_presence_of :last_name
   validates_presence_of :email
-  has_one :building
+  has_one :building, dependent: :nullify
   has_one :building_ownership, through: :building
 end
